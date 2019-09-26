0f2b569de1a7aa62e4726d057e7b10d312f85495
System Administrator <admin@example.com>
2019-09-26 02:33:13 -0400

    Changes by 'System Administrator' on 'TestDevopsApp – 1.0.0' -- Build #1

---
+++ sn/TestDevopsApp/Dictionary Entry/incident/active_true/u_location_1/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : TestDevopsApp
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-09-06 09:28:53
+ * Created By  : admin
+ * Updated On  : 2019-09-06 09:28:53
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=5d518bd8db33330011c27a6139961950
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
